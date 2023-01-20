d167d573765c440bd1914c4ae561a373f755656e
AndrewTCarter <Andrewkarter@gmail.com>
2023-01-20 15:54:50 -0500

    commit tree 94bb55d4d5dfb21aa74dc39e06bac8ffd4c852dc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleInput(e.target[0].value)
+    form.reset()
+  })
 });
+
+function handleInput(task){
+  const p = document.createElement('p')
+  p.innerText = `${task} `
+  const btn = document.createElement('button')
+  btn.addEventListener('click', deleteButton)
+  btn.innerText = ' X '
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function deleteButton(e) {
+  e.target.parentNode.remove()
+}
